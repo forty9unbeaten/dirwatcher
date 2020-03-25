9b8ac575b6b00e4ffba628211153d5c9d4958be4
Rob Spears <rob.sp3ars@gmail.com>
2020-03-25 18:16:17 -0400

    Add optional argument for logging level

--- dirwatcher.py
+++ dirwatcher.py
@@ -17,17 +17,29 @@ def create_parser(args):
     '''Create and return a command-line argument parser'''
     parser = argparse.ArgumentParser(
         description='Monitors the creation of files with a specified ' +
-        'extension and searches those files for a string of specified text')
+        'extension and searches those files for a string of specified text',
+        formatter_class=argparse.RawTextHelpFormatter,
+        usage='%(prog)s [-h] [-i,--interval] ' +
+        '[-t,--magictext][-l,--loglevel] dir ext')
     parser.add_argument('dir',
                         help='The directory to monitor')
     parser.add_argument('ext',
                         help='The file extension on which to filter')
     parser.add_argument('-i', '--interval',
-                        help='polling interval (defaults to 1 second',
+                        help='polling interval (defaults to 1 second)',
+                        metavar='',
                         dest='pollint')
-    parser.add_argument('-t', '--magictext',
+    parser.add_argument('-t', '--text',
                         help='The string of text for which to monitor',
+                        metavar='',
                         dest='text')
+    parser.add_argument('-l', '--loglevel',
+                        help='number that determines program log ' +
+                        'verbosity\n\t1 - DEBUG (default)\n\t2 - INFO\n' +
+                        '\t3 - WARNING\n\t4 - ERROR\n\t5 - CRITICAL',
+                        metavar='',
+                        dest='loglevel',
+                        default=1)
 
     # print help if no args are supplied on command-line
     if not args:
