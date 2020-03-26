aaad2e3781bad804c2066b87344496cda891438b
Rob Spears <rob.sp3ars@gmail.com>
2020-03-26 15:53:40 -0400

    Write functions that check for both new and deleted files in the directory and add/remove them from cache

--- dirwatcher.py
+++ dirwatcher.py
@@ -9,6 +9,8 @@ from datetime import datetime as dt
 import os
 import textwrap
 import signal
+import re
+import time
 
 if sys.version_info[0] < 3:
     print('\n\tSincerest apologies, gotta use Python3\n')
@@ -34,19 +36,19 @@ def create_parser(args):
         'extension and searches those files for a string of specified text',
         formatter_class=argparse.RawTextHelpFormatter,
         usage='%(prog)s [-h] [-i,--interval] ' +
-        '[-t,--magictext][-l,--loglevel] dir ext')
+        '[-l,--loglevel] dir ext text')
     parser.add_argument('dir',
                         help='The directory to monitor')
     parser.add_argument('ext',
                         help='The file extension on which to filter')
+    parser.add_argument('text',
+                        help='The string of text for which to monitor',)
     parser.add_argument('-i', '--interval',
                         help='polling interval (defaults to 1 second)',
                         metavar='',
-                        dest='pollint')
-    parser.add_argument('-t', '--text',
-                        help='The string of text for which to monitor',
-                        metavar='',
-                        dest='text')
+                        dest='pollint',
+                        type=float,
+                        default=1.0)
     parser.add_argument('-l', '--loglevel',
                         help='number that determines program log ' +
                         'verbosity\n\t1 - DEBUG (default)\n\t2 - INFO\n' +
@@ -147,6 +149,84 @@ def signal_handler(sig_num, frame):
     exit_flag = True
 
 
+def watch_directories(directory, file_ext, magic_text, interval):
+    '''
+    Monitor files in a specific directory for a specific string of text
+
+    Parameters:
+
+    directory: The directory to monitor
+    file_ext: extension of the files in which to look for text string
+    magic_text: the string of text for which to look
+    interval: the rate at which the function looks in the directory
+    for the text
+
+    Return: None
+    '''
+    # regular expression to isolate files with specified extension
+    extension_regex = r'\S*{}\Z'.format(file_ext)
+
+    # dictionary that tracks files that have been read
+    # keys are filenames and values are number of lines already read
+    file_cache = {}
+    directory_path = os.path.abspath(directory)
+    logger.info('Monitored Directory: {}, File Ext: {},'.format(directory_path,
+                                                                file_ext)
+                + ' Text: {}, Interval: {}'.format(magic_text, interval))
+
+    while not exit_flag:
+        logger.info('Getting list of files with extension {} in {}'.format(
+            file_ext, directory_path))
+        files = [file for file in os.listdir(
+            directory_path) if re.search(extension_regex, file)]
+
+        # add and remove appropriate files from cache dictionary
+        logger.info('Checking for new files in {}'.format(directory_path))
+        file_cache = sync_added_files(file_cache, files)
+        logger.info('Checking for deleted files in {}'.format(directory_path))
+        file_cache = sync_deleted_files(file_cache, files)
+
+        # delay next polling in accordance with interval
+        time.sleep(interval)
+
+
+def sync_added_files(cache, files):
+    '''
+    Checks for new files that are not currently being
+    tracked by the cache dictionary
+
+    Parameters:
+    cache: the dictionary of files currently being tracked
+    files: the list of files in the directory
+
+    Return: an updated version of the cache dictionary with new files
+    '''
+    for file in files:
+        if file not in cache.keys():
+            cache[file] = 0
+            logger.info('Added {} to file cache'.format(file))
+    return cache
+
+
+def sync_deleted_files(cache, files):
+    '''
+    Compares files stored in cache dictionary that are no longer
+    included in the directory and deletes them.
+
+    Parameters:
+    cache: the existing cache dictionary
+    files: the list of files in the directory
+
+    Return: an updated version of the cache dictionary
+    without deleted files
+    '''
+    for cached_file in cache.keys():
+        if cached_file not in files:
+            cache.pop(cached_file, None)
+            logger.info('Deleted {} from file cache'.format(cached_file))
+    return cache
+
+
 def main(args):
     # create parser for shell arguments
     parser = create_parser(args)
@@ -166,6 +246,9 @@ def main(args):
     --------------------------------'''.format(prog_start_time, os.getpid())))
     # connect signal handlers
     config_signal_handlers()
+
+    # monitor the directory for the string of text
+    watch_directories(ns.dir, ns.ext, ns.text, ns.pollint)
 
     # banner log to indicate program end
     prog_end_time = dt.now()
